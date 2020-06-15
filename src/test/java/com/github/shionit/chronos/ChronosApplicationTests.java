95b0c3c802efb77f7657764721c1d8c3f5cf6a9a
shionit <frontier.engine@gmail.com>
2020-06-15 09:34:08 +0900

    add PMD check

--- src/test/java/com/github/shionit/chronos/ChronosApplicationTests.java
+++ src/test/java/com/github/shionit/chronos/ChronosApplicationTests.java
@@ -3,6 +3,9 @@ package com.github.shionit.chronos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * ChronosApplicationTests
+ */
 @SpringBootTest
 class ChronosApplicationTests {
 
