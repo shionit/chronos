319f97ff9d33d5e5740f037a2292a9a4797bb85a
shionit <frontier.engine@gmail.com>
2020-07-28 22:32:32 +0900

    add Flyway migration #10

--- src/main/java/com/github/shionit/chronos/usecase/BookService.java
+++ src/main/java/com/github/shionit/chronos/usecase/BookService.java
@@ -2,10 +2,8 @@ package com.github.shionit.chronos.usecase;
 
 import com.github.shionit.chronos.model.Book;
 import com.github.shionit.chronos.model.repository.BookRepository;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,15 +19,6 @@ public class BookService {
    * BookRepository
    */
   private final BookRepository bookRepository;
-
-  /**
-   * mock data
-   */
-  private static final Map<Long, Book> MOCK_BOOKS = ImmutableMap.of(
-//      1L, Book.builder().id(1L).title("This is me.").isbn("123-444-56").build(),
-//      2L, Book.builder().id(2L).title("Hello world").isbn("246-777-89").build(),
-//      3L, Book.builder().id(3L).title("Apple pen").isbn("369-000-54").build()
-  );
 
   /**
    * 書籍を取得する。
